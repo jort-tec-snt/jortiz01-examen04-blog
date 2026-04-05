a24fbb5b286a0abfb6968e8cd851d9a30e250a53
jort-tec-snt <jeronimo.ortiz@tecsup.edu.pe>
2026-04-05 16:09:40 -0500

    agregando modelos autor y articulo bajo un foreign key de 1 a muchos

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo_electronico = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
